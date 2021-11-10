038ea30cac8e8007dded1e67eb182f5f77faadc8
jaceiverson <iverson.jace@gmail.com>
2021-11-10 16:26:48 -0500

    added ability to get header and also get body and header as strings

--- src/googlewrapper/docs.py
+++ src/googlewrapper/docs.py
@@ -1,35 +1,51 @@
-from .connect import Connection
+from googlewrapper.connect import Connection
+from typing import Optional
 
 
 class GoogleDocs:
-    def __init__(self, doc_id = None) -> None:
+    def __init__(self, doc_id:Optional[str] = None) -> None:
         self.id = doc_id
         self.connection = Connection().docs()
         if doc_id:
-            self.doc = self._get_doc()
+            self.doc: dict = self._get_doc()
 
-    def set_id(self,doc_id:str):
+    def set_id(self,doc_id:str) -> None:
         """Sets the ID to new ID, calls the API to get the new doc"""
         self.id = doc_id
         self.doc = self._get_doc()
 
-    def _get_doc(self):
+    def _get_doc(self) -> dict:
         """Returns the dictionary object of the Google Doc"""
         return self.connection.documents().get(documentId=self.id).execute()
 
-    def get_title(self):
+    def get_title(self) -> str:
         """Returns the title of the Google Doc"""
         return self.doc.get('title')
 
-    def get_id(self):
+    def get_id(self) -> str:
         """Returns the ID of the Google Doc"""
         return self.id
 
-    def get_body(self):
-        """Returns the Body of the Google Doc"""
-        return self.doc.get('body')
+    def get_header(self) -> list[dict]:
+        """Returns the Header of the Google Doc"""
+        return self.doc.get('headers').get(list(self.doc.get('headers').keys())[0]).get('content')
 
-    def combine_content(self,content:dict):
+    def get_body(self) -> list[dict]:
+        """Returns the Body of the Google Doc"""
+        return self.doc.get('body').get('content')
+
+    def get_text(self,doc_object:list[dict]) -> str:
+        """
+        Returns the text values of the entire document body as a string
+        It is reccomended that you pass in 
+        
+            self.get_header() or self.get_body()
+        
+        as the doc_object for best results
+        """
+        return '\n'.join([self.combine_content(x) for x in doc_object if 'paragraph' in x.keys()])
+
+    def combine_content(self,content:dict) -> str:
         """Accepts a dictionary from Google Docs, this will be a 
         list element from the body (self.get_body()) of the Google Doc
         
